4ed50c475946cbb8c661f91ac76d861dcdaee902
Shivam <thisisshivam06@gmail.com>
2022-07-17 19:36:24 +0530

    Sending a response

--- server.js
+++ server.js
@@ -1,8 +1,32 @@
+// const http = require("http");
+
+// const server = http.createServer((req, res) => {
+//   res.writeHead(200, { "Content-type": "text/plain" });
+//   res.end("Hello dude, i am a response");
+// });
+
+// server.listen(8181, "127.0.0.1");
+// console.log("Server is listening on port 8181");
+
+////////////////// SENDING A RESPONSE ////////////////
+
 const http = require("http");
+const fs = require("fs");
+
+// let HTML = fs.readFileSync(`${__dirname}/index.html`);
+const names = ["francis", "james", "Rob"];
+const cars = {
+  name: "Ford",
+  model: "Fiesta",
+};
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-type": "text/plain" });
-  res.end("Hello dude, i am a response");
+  res.writeHead(200, { "Content-Type": "application/json" });
+  const json = JSON.stringify({
+    names,
+    cars,
+  });
+  res.end(json);
 });
 
 server.listen(8181, "127.0.0.1");
