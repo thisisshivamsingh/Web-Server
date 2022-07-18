48c40852e02e79388d32574ea6956bf7b8a41cad
Shivam <thisisshivam06@gmail.com>
2022-07-18 17:34:20 +0530

    Middleware 1

--- server.js
+++ server.js
@@ -92,8 +92,51 @@
 
 ///////////////// QUERY STRINGS AND PARAMS ///////////////
 
+// const express = require("express");
+// const app = express();
+
+// app.get("/api/:user/:id", (req, res) => {
+//   let id = req.params.id;
+//   let user = req.params.user;
+//   res.send(
+//     `<html>
+//       <body>
+//         <h1>The User name is ${user} and the id is ${id}</h1>
+//       </body>
+//     </html>`
+//   );
+// });
+
+// /// hhh.com/car?brand=ford&year=2022
+// app.get("/api/car", (req, res) => {
+//   let brand = req.query.brand;
+//   let year = req.query.year;
+//   res.send({ brand, year });
+// });
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT);
+
+/////////////////// MIDDLEWARE 1 //////////////////
+
 const express = require("express");
 const app = express();
+
+app.use("/css", express.static(__dirname + "/public/css"));
+
+app.get("/", (req, res) => {
+  res.send(
+    `<html>
+      <head>
+        <link type="text/css" rel="stylesheet" href="/css/styles.css">
+      </head>
+      <body>
+        <h1>
+          Hello !!
+        </h1>
+      </body>
+    </html>`
+  );
+});
 
 app.get("/api/:user/:id", (req, res) => {
   let id = req.params.id;
