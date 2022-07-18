650cf57b7dca71927aaad2cfbcab8d802bf0c8ef
Shivam <thisisshivam06@gmail.com>
2022-07-18 14:14:38 +0530

    Creating a server with Express

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+///////////////// CREATING A NODE WEB SERVER ///////////////
+
 // const http = require("http");
 
 // const server = http.createServer((req, res) => {
@@ -34,32 +36,56 @@
 
 ////////////////// CREATING ROUTES /////////////////
 
-const http = require("http");
-const fs = require("fs");
+// const http = require("http");
+// const fs = require("fs");
 
-const names = ["francis", "james", "Rob"];
-const cars = {
-  name: "Ford",
-  model: "Fiesta",
-};
+// const names = ["francis", "james", "Rob"];
+// const cars = {
+//   name: "Ford",
+//   model: "Fiesta",
+// };
 
-const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    let HTML = fs.readFileSync(`${__dirname}/index.html`);
-    res.end(HTML);
-  } else if (req.url === "/api/user") {
-    res.writeHead(200, { "Content-Type": "application/json" });
-    const json = JSON.stringify({
-      names,
-      cars,
-    });
-    res.end(json);
-  } else {
-    res.writeHead(400);
-    res.end();
-  }
+// const server = http.createServer((req, res) => {
+//   if (req.url === "/") {
+//     res.writeHead(200, { "Content-Type": "text/html" });
+//     let HTML = fs.readFileSync(`${__dirname}/index.html`);
+//     res.end(HTML);
+//   } else if (req.url === "/api/user") {
+//     res.writeHead(200, { "Content-Type": "application/json" });
+//     const json = JSON.stringify({
+//       names,
+//       cars,
+//     });
+//     res.end(json);
+//   } else {
+//     res.writeHead(400);
+//     res.end();
+//   }
+// });
+
+// server.listen(8181, "127.0.0.1");
+// console.log("Server is listening on port 8181");
+
+///////////////// CREATING A SERVER WITH EXPRESS ///////////////
+
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send(
+    `<html>
+      <body>
+        <h1>Hello !!</h1>
+      </body>
+    </html>`
+  );
 });
 
-server.listen(8181, "127.0.0.1");
-console.log("Server is listening on port 8181");
+app.get("/api/user", (req, res) => {
+  res.send({
+    name: "Francis",
+    lastname: "Jones",
+  });
+});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT);
